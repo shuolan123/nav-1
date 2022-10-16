bcbb84ee560a85e1f7ad1db9db2f432addd79480
vansking <a867852032@gmail.com>
2022-10-16 10:39:34 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,67 @@
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x') //在缓存中寻找目标对象
+const xObject = JSON.parse(x)    // 字符串转对象
+const hashMap = xObject || [
+    {logo:"A",url:"https://www.acfun.cn"},
+    {logo:"B",url:"https://www.bilibili.com"}
+]
+const simplifyUrl = (url) => {
+    return url.replace('https://','')
+        .replace('http://','')
+        .replace('www','')
+        .replace(/\/.*/,'') //删除以/开头的内容
+}
+const render = () => {
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node,index) =>{
+        const $li = $(`<li>
+            <div class="site">
+                <div class="logo">${node.logo}</div>
+                <div class="link">${simplifyUrl(node.url)}</div>
+                <div class="close">
+                    <svg class="icon">
+                        <use xlink:href="#icon-close"></use>
+                    </svg>
+                </div>
+            </div>
+        </li>`).insertBefore($lastLi)
+        $li.on('click', () =>{
+        window.open(node.url)
+        })
+        $li.on('click','.close',(e)=>{
+            e.stopPropagation() //阻止冒泡
+            hashMap.splice(index,1)
+            render()
+        })
+    })
+}
+
+render()
+
+$('.addButton').on('click',()=>{
+    let url = window.prompt('请输入你想要添加的网址')
+    if(url.indexOf('http') !== 0){
+        url = 'https://' + url
+    }
+    console.log(url)
+    hashMap.push({
+        logo:simplifyUrl(url)[0].toUpperCase(),
+        url:url
+    })
+    render()
+})
+
+window.onbeforeunload  = () =>{   //监听用户的退出行为
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x',string)
+}
+
+$(document).on('keypress',(e) =>{
+    const {key} = e
+    for(let i=0;i<hashMap.length;i++){
+        if(hashMap[i].logo.toLowerCase() === key){
+            window.open(hashMap[i].url)
+        }
+    }
+})
